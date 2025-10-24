a95937c3520568c42e57f6e8a06b3414d452db7b
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2025-10-25 02:17:32 +0530

    Add files via upload

---
+++ exercise_4/4d/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  standalone: false,
+  styleUrl: './app.css'
+})
+export class App {
+  // protected readonly title = signal('4d');
+  title = 'product details'; 
+  productCode = 'PROD_P001'; 
+  productName = 'Laptop'; 
+}
