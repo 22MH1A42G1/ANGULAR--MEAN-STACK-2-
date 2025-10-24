40140eaa2a30e6e50ecbf49b85797b58dca6ea8b
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2025-10-25 02:11:34 +0530

    Add files via upload

---
+++ exercise_4/4c/src/app/app-module.ts
@@ -0,0 +1,23 @@
+import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+
+@NgModule({
+  declarations: [
+    App
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
